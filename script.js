dc5cf3898d2123d118ed818ef69626b0e32e84b4
BoasOrlik <boas.orlik@stud.fhgr.ch>
2023-11-14 22:14:33 +0100

    js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function greet() {
     alert("Hallo, Welt!");
 }
+
+
+// JavaScript-Funktion zum automatischen Scrollen
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
